5bb92b90347d27c5a9306c095b0431861056ad6a
kvlinden <kvlinden@calvin.edu>
2025-07-11 13:12:12 -0400

    updated node version cleaned up documentation and formatting

--- monopolyService.js
+++ monopolyService.js
@@ -1,41 +1,41 @@
-/* eslint-disable no-template-curly-in-string */
-/* eslint-disable no-console */
-/* eslint-disable no-use-before-define */
 /**
  * This module implements a REST-inspired web service for the Monopoly DB.
- * The database is hosted on PostgreSQL for Azure.
+ * The database is hosted on PostgreSQL for Azure. Implementation notes:
  *
- * Currently, the service is written in JS and supports the player table only.
+ * - Currently, the service is written in JS and supports the Player table only.
  *
- * To guard against SQL injection attacks, this code uses pg-promise's built-in
- * variable escaping. This prevents a client from issuing this URL:
+ * - The service assumes that the database connection strings and the server
+ * mode are set in environment variables. See the DB_* variables used by
+ * pg-promise.
+ *
+ * - To guard against SQL injection attacks, this code uses pg-promise's built-in
+ * variable escaping. This prevents a client from issuing this SQL-injection URL:
  *     https://cs262-webservice.azurewebsites.net//players/1%3BDELETE%20FROM%20PlayerGame%3BDELETE%20FROM%20Player
  * which would delete records in the PlayerGame and then the Player tables.
- * In particular, we don't use JS template strings because it doesn't filter
+ * In particular, we don't use JS template strings because this doesn't filter
  * client-supplied values properly.
- * TODO: Consider using Prepared Statements.
- *      https://vitaly-t.github.io/pg-promise/PreparedStatement.html
  *
- * This service assumes that the database connection strings and the server mode
- * are set in environment variables. See the DB_* variables used by pg-promise.
- * And setting NODE_ENV to production will cause ExpressJS to serve up uninformative
- * server error responses for all errors.
+ * - The endpoints call `next(err)` to handle errors so that the service doesn't
+ * crash. This initiates the default error handling middleware, which logs full
+ * error details to the server-side console and returns uninformative HTTP 500
+ * responses to clients. This makes the service a bit more secure (because it
+ * doesn't reveal database details to clients), but also makes it more difficult
+ * for API users (because they don't get useful error messages).
  *
  * @author: kvlinden
  * @date: Summer, 2020
- * @date: Fall, 2025 (updated for current Azure/Node versions)
+ * @date: Fall, 2025 (updated documentation and Node version)
  */
 
 // Set up the database connection.
-
 const pgp = require('pg-promise')();
 
 const db = pgp({
-  host: process.env.DB_SERVER,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 });
 
 // Configure the server and its routes.
@@ -55,68 +55,71 @@ router.post('/players', createPlayer);
 router.delete('/players/:id', deletePlayer);
 
 app.use(router);
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Implement the CRUD operations.
 
-function returnDataOr404(res, data) {
-  if (data == null) {
-    res.sendStatus(404);
-  } else {
-    res.send(data);
-  }
+function returnDataOr404(response, data) {
+    if (data == null) {
+        response.sendStatus(404);
+    } else {
+        response.send(data);
+    }
 }
 
 function readHelloMessage(req, res) {
-  res.send('Hello, CS 262 Monopoly service!');
+    res.send('Hello, CS 262 Monopoly service!');
 }
 
-function readPlayers(req, res, next) {
-  db.many('SELECT * FROM Player')
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      next(err);
-    });
+function readPlayers(request, response, next) {
+    db.manyOrNone('SELECT * FROM Player')
+        .then((data) => {
+            // data is a list, never null, so returnDataOr404 isn't needed.
+            response.send(data);
+        })
+        .catch((error) => {
+            next(error);
+        });
 }
 
-function readPlayer(req, res, next) {
-  db.oneOrNone('SELECT * FROM Player WHERE id=${id}', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
+function readPlayer(request, response, next) {
+    db.oneOrNone('SELECT * FROM Player WHERE id=${id}', request.params)
+        .then((data) => {
+            returnDataOr404(response, data);
+        })
+        .catch((error) => {
+            next(error);
+        });
 }
 
-function updatePlayer(req, res, next) {
-  db.oneOrNone('UPDATE Player SET email=${body.email}, name=${body.name} WHERE id=${params.id} RETURNING id', req)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
+function updatePlayer(request, response, next) {
+    db.oneOrNone('UPDATE Player SET email=${body.email}, name=${body.name} WHERE id=${params.id} RETURNING id', request)
+        .then((data) => {
+            returnDataOr404(response, data);
+        })
+        .catch((error) => {
+            next(error);
+        });
 }
 
-function createPlayer(req, res, next) {
-  db.one('INSERT INTO Player(email, name) VALUES (${email}, ${name}) RETURNING id', req.body)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      next(err);
-    });
+function createPlayer(request, response, next) {
+    db.one('INSERT INTO Player(email, name) VALUES (${email}, ${name}) RETURNING id', request.body)
+        .then((data) => {
+            // New players are always created, so returnDataOr404 isn't needed.
+            response.send(data);
+        })
+        .catch((error) => {
+            next(error);
+        });
 }
 
-function deletePlayer(req, res, next) {
-  db.oneOrNone('DELETE FROM Player WHERE id=${id} RETURNING id', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
+function deletePlayer(request, response, next) {
+    db.oneOrNone('DELETE FROM Player WHERE id=${id} RETURNING id', request.params)
+        .then((data) => {
+            returnDataOr404(response, data);
+        })
+        .catch((error) => {
+            next(error);
+        });
 }
